1106641657f08d41cecf66152508f3a26fa1aeda
Mohue <mohue03@gmail.com>
2022-07-14 18:52:23 +0300

    Added playRound() function

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,7 +1,35 @@
+// Generates random number from 1 - 3 to determine the computer will play
 computerPlay = () => (
     Math.floor(Math.random() * 3) + 1
 );
 
-for (let i = 0; i < 15; i++) {
-    console.log(computerPlay());
+//Takes in both selections and determines which selection is the winner/loser
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    //If both inputs are the same, return 'tie' value
+    if (playerSelection === computerSelection) {
+        result = 'tie';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            result = 'win';
+        } else if (computerSelection === 'paper') {
+            result = 'lose';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            result = 'win';
+        } else if (computerSelection === 'scissors') {
+            result = 'lose';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            result = 'lose';
+        } else if (computerSelection === 'paper') {
+            result = 'win';
+        }
+    }
+    return result;
 }
+
+console.log(playRound('rock','scissors'));
